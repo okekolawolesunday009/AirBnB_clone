4bb96c61a7c180a74a88c350cb98207a87d1ff77
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-10-13 14:52:31 +0100

    code

--- models/base_model.py
+++ models/base_model.py
@@ -15,20 +15,20 @@ class BaseModel:
         """
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'id':
+                if key != '__class__':
+                    setattr(self, key, value)
+                elif key == 'updated_at':
+                    dated = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.updated_at = dated
+                elif key == 'id':
                     self.id = value
                 elif key == 'created_at':
                     dated = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     self.created_at = dated
-                elif key == 'updated_at':
-                    dated = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    self.updated_at = dated
-                elif key != '__class__':
-                    setattr(self, key, value)
         else:
+            self.updated_at = self.created_at
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
             from models import storage
             storage.new(self)
     """str"""
