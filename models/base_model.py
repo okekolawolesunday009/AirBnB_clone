631db2231581b6ee1d4e291ad675680f05644f8b
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-10-14 07:53:04 +0100

    code

--- models/base_model.py
+++ models/base_model.py
@@ -43,8 +43,8 @@ class BaseModel:
 
     def to_dict(self):
         """Returns a dictionary representation of the instance."""
-        obj_dict["__class__"] = self.__class__.__name__
         obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
         obj_dict["updated_at"] = self.updated_at.isoformat()
         return obj_dict
