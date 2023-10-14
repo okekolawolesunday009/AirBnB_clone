8326c59244f22cb54fccd1d6f60f2e6abccee13a
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-10-14 14:43:39 -0700

    py

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Defines a base class"""
-import uuidi
-from models import storage
+import uuid
 from datetime import datetime
 
 
@@ -30,6 +29,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            from models import storage 
             storage.new(self)
     """str"""
     def __str__(self):
@@ -40,6 +40,7 @@ class BaseModel:
     def save(self):
         """time saved"""
         self.updated_at = datetime.now()
+        from models import storage
         storage.save()
 
     def to_dict(self):
