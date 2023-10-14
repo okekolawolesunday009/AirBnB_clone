33ac9386bf56da4bff1203de2f8d55477facecc6
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-10-14 14:39:29 -0700

    code

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a base class"""
-import uuid
+import uuidi
+from models import storage
 from datetime import datetime
 
 
@@ -10,7 +11,7 @@ class BaseModel:
         """
         Update attributes using positional arguments.
         Args:
-            args: Positional arguments in the order id, 
+            args: Positional arguments in the order id,
             craeted_at, updated_at, __class__.
         """
         if kwargs:
@@ -29,9 +30,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            from models import storage 
             storage.new(self)
-            
+    """str"""
     def __str__(self):
         """string documentation"""
         return "[{}] ({}) {}".format(
@@ -40,7 +40,6 @@ class BaseModel:
     def save(self):
         """time saved"""
         self.updated_at = datetime.now()
-        from models import storage 
         storage.save()
 
     def to_dict(self):
