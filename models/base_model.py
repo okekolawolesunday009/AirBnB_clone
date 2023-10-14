2bb27fbb1666534528a15b48b520a0ac77155394
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-10-14 07:54:28 +0100

    code

--- models/base_model.py
+++ models/base_model.py
@@ -15,14 +15,16 @@ class BaseModel:
         """
         if kwargs:
             for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
+                if key == 'id':
+                    self.id = value
                 elif key == 'created_at':
                     dated = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     self.created_at = dated
-                elif key == 'updated_at' or key == "created_at":
-                    self.__dict__[key] = value
-                    
+                elif key == 'updated_at':
+                    dated = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.updated_at = dated
+                elif key != '__class__':
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -44,7 +46,7 @@ class BaseModel:
     def to_dict(self):
         """Returns a dictionary representation of the instance."""
         obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
         obj_dict["updated_at"] = self.updated_at.isoformat()
+        obj_dict["__class__"] = self.__class__.__name__
         return obj_dict
